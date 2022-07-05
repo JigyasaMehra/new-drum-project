7ee745a57bdf284dca0062ee9c6c9e039a1375a8
JigyasaMehra <91827239+JigyasaMehra@users.noreply.github.com>
2022-07-05 17:11:43 +0530

    Update drum.js

--- drum.js
+++ drum.js
@@ -12,37 +12,37 @@ document.addEventListener("keypress",function (event) {
 });
  function soundMake(key){
   switch (key) {
-    case "w":
+    case "j":
     var audio = new Audio('tom-1.mp3');
         audio.play();
 
       break;
-      case "a":
+      case "i":
          var audio1 = new Audio('tom-2.mp3');
          audio1.play();
 
          break;
-      case "s":
+      case "g":
          var audio2 = new Audio('tom-3.mp3');
          audio2.play();
 
          break;
-      case "d":
+      case "y":
          var audio3 = new Audio('tom-4.mp3');
          audio3.play();
 
         break;
-      case "j":
+      case "a":
         var audio4 = new Audio('snare.mp3');
            audio4.play();
 
            break;
-      case "k":
+      case "s":
       var audio5 = new Audio('kick-bass.mp3');
          audio5.play();
 
           break;
-      case "l":
+      case "w":
           var audio6 = new Audio('crash.mp3');
              audio6.play();
              break;
